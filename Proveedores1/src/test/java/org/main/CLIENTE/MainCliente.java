99daa23fc6bd46ddf4edf3b70860157aec8efe43
Henry Arevalo <henry.arevalo@uptc.edu.co>
2024-05-10 21:11:56 -0500

    Update MainCliente.java

--- Proveedores1/src/test/java/org/main/CLIENTE/MainCliente.java
+++ Proveedores1/src/test/java/org/main/CLIENTE/MainCliente.java
@@ -11,12 +11,12 @@ public class MainCliente {
 
 			System.out.println("Se ingresaran los valores del arreglo local");
 			int[] arregloLocal = new int[6];
-			arregloLocal[0] = 5;
-			arregloLocal[1] = 9;
-			arregloLocal[2] = 5;
-			arregloLocal[3] = 9;
+			arregloLocal[0] = 1;
+			arregloLocal[1] = 2;
+			arregloLocal[2] = 3;
+			arregloLocal[3] = 4;
 			arregloLocal[4] = 5;
-			arregloLocal[5] = 9;
+			arregloLocal[5] = 6;
 
 			Registry registroCliente = LocateRegistry.getRegistry("192.168.20.31",2024);
 			RemoteInterface rmi = (RemoteInterface) registroCliente.lookup("Ventas");
